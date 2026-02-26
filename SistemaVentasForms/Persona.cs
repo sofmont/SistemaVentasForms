727cd3690f8459d870663dcbb2200e0f86c9dc9a
sofmont <barbaramontalvo01234@gmail.com>
2026-02-25 22:02:27 -0600

    Agregar archivos de proyecto.

---
+++ SistemaVentasForms/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentasForms
+{
+    internal class Persona
+    {
+
+        // Atributos privados
+        private string nombre;
+        private string telefono;
+        private string correo;
+        private string direccion;
+        private string ciudad;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+      
+
+
+        public Persona(string nombre, string telefono)
+        {
+            this.nombre = nombre;
+            this.telefono = telefono;
+          
+        }
+
+        public override string ToString()
+        {
+            return $"{nombre} - {telefono}";
+        }
+    }
+}
---
+++ SistemaVentasForms/Producto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentasForms
+{
+    internal class Producto
+    {
+        private int id;
+        private string nombre;
+        private double precio;
+        private int stock;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public double Precio { get => precio; set => precio = value; }
+        public int Stock { get => stock; set => stock = value; }
+
+        public Producto(int id, string nombre, double precio, int stock)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.precio = precio;
+            this.stock = stock;
+        }
+
+       public override string ToString()
+        {
+            return $"{id} - {nombre} - ${precio} - Stock: {stock}";
+        }
+    }
+}
